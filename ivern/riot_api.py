0e88b5ca2f798a3c292988b04f584608500e786b
Geoffrey Houy <geoffhouy@gmail.com>
2017-05-06 13:45:13 -0400

    Add get_static_runes_data

--- ivern/riot_api.py
+++ ivern/riot_api.py
@@ -228,6 +228,16 @@ class RiotAPI(object):
             params=params
         )
 
+    def get_static_runes_data(self, params={}):
+        return self._request(
+            url=URL['static_data'].format(
+                version=VERSION['static_data'],
+                category='runes'
+            ),
+            region=None,
+            params=params
+        )
+
     def get_summoner_by_name(self, region, name):
         return self._request(
             url=URL['summoner_by_name'].format(
