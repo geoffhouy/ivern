41c11e1adad12e67f2d9b00a3965148b496b8444
Geoffrey Houy <geoffhouy@gmail.com>
2017-05-06 13:45:33 -0400

    Add get_static_summoner_spells_data

--- ivern/riot_api.py
+++ ivern/riot_api.py
@@ -238,6 +238,16 @@ class RiotAPI(object):
             params=params
         )
 
+    def get_static_summoner_spells_data(self, params={}):
+        return self._request(
+            url=URL['static_data'].format(
+                version=VERSION['static_data'],
+                category='summoner-spells'
+            ),
+            region=None,
+            params=params
+        )
+
     def get_summoner_by_name(self, region, name):
         return self._request(
             url=URL['summoner_by_name'].format(
